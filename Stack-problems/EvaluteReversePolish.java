f51ab65b1c712a27ef8807002aa24ef173afa7d9
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-14 01:50:52 +0530

    Add evalute RPN solution

---
+++ Stack-problems/EvaluteReversePolish.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0; i < tokens.length; i++) {
+
+            String token = tokens[i];
+
+            if(!token.equals("+") &&
+               !token.equals("-") &&
+               !token.equals("*") &&
+               !token.equals("/")) {
+
+                stack.push(Integer.parseInt(token));
+            }
+
+            else {
+
+                int a = stack.pop();
+                int b = stack.pop();
+
+                int result;
+
+                if(token.equals("+"))
+                    result = b + a;
+                else if(token.equals("-"))
+                    result = b - a;
+                else if(token.equals("*"))
+                    result = b * a;
+                else
+                    result = b / a;
+
+                stack.push(result);
+            }
+        }
+
+        return stack.peek();
+    }
+}
