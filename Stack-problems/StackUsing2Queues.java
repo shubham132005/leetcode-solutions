138f5b14ba1017d1457a9363f174172f7a46a6af
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-17 14:16:17 +0530

    stack using 2 queues

--- Stack-problems/StackUsing2Queues.java
+++ Stack-problems/StackUsing2Queues.java
@@ -1,14 +1,36 @@
-public class StackUsing2Queues {
-    
-    Queue<Integer> q1 = new LinkedList<>();
-    Queue<Integer> q2 = new LinkedList<>();
+import java.util.*;
 
+class MyStack {
 
-    void push(int data){
-        q2.add(data);
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
-    while(!q1.isEmpty()){
-        q2.offer(q1.poll());
+
+    public void push(int x) {
+        q2.offer(x);
+
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
 
+    public int pop() {
+        return q1.poll();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
 }
