a03681c0f1e2462c80ccb5b719161c887903e697
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-19 02:17:41 +0530

    maximum subarray solution

---
+++ Arrays/MaximumSubarray.java
@@ -0,0 +1,15 @@
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums){
+        int max = Integer.MIN_VALUE;
+        int curr = 0;
+        for(int num : nums){
+            curr += num;
+            max = Math.max(curr,max);
+            if(curr < 0){
+                curr = 0;
+            }
+        }
+        return max;
+    }
+    
+}
