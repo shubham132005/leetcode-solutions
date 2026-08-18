ee868e5433dedcced952af9e88a36549823acf53
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-19 01:14:43 +0530

    majority element solution

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,19 @@
+public class MajorityElement {
+    public int majorityElement(int[] nums){
+        int candidate = 0;
+        int count = 0;
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
+                count = 1;
+            }
+            else if(candidate == num){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate;
+    }
+    
+}
