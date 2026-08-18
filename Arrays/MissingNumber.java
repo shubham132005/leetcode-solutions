251c89a7d477b55b79f5b6da568cd5d8ec81c19e
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-18 21:30:48 +0530

    missing number solution

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,12 @@
+public class MissingNumber {
+    public int missingNumber(int[] nums){
+        int xor = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            xor ^= i;
+        }
+        for(int num : nums){
+            xor ^= num;
+        }
+        return xor;
+    }
+}
