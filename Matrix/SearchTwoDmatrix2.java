cb6a1afdf0f1114db1574fe923b745ae12970c6f
Shubham Sharma <shubhamdongriyal07@gmail.com>
2026-08-20 01:41:07 +0530

    search a 2D matrix 2 solution

---
+++ Matrix/SearchTwoDmatrix2.java
@@ -0,0 +1,21 @@
+package Matrix;
+
+public class SearchTwoDmatrix2 {
+    public boolean searchMatrix(int[][] matrix, int target){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = n-1;
+        while(row <= m-1 && col>= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }else{
+                col--;
+            }
+        }
+        return false;
+    }
+}
